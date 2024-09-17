b8cf06463ca5999a65ae6808b2fdca671575f38f
Kamali <kamali@matryxsoft.com>
2024-09-17 12:52:49 +0530

    made changes in testbase

--- src/main/java/com/workday/reusables/LoginPage.java
+++ src/main/java/com/workday/reusables/LoginPage.java
@@ -1,5 +1,42 @@
 package com.workday.reusables;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+
+
+public class LoginPage extends Testbase {
+
+	/**
+	 * Here, we are locating the objects from the application utilizing the Object
+	 * repository or Page Factory Framework.
+	 */
+
+	// page factory -Object Repository
+	// Object Repository for Username
+	@FindBy(xpath = "//input[@aria-label='Username']")
+	WebElement username;
+
+	// Object Repository for Password
+	@FindBy(xpath = "//input[@aria-label='Password']")
+	WebElement password;
+
+	// Object Repository for Sign In
+	@FindBy(xpath = "//button[normalize-space()='Sign In']")
+	WebElement SignIn;
+
+	// Object Repository for Skip
+	@FindBy(xpath = "//a[normalize-space()='Skip']")
+	WebElement Skip;
+
+	// Initilizing the page object
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+
+	// Actions
+	public String LoginVerification() {
+		return driver.getTitle();
+	}
 }
--- src/main/java/com/workday/reusables/Testbase.java
+++ src/main/java/com/workday/reusables/Testbase.java
@@ -1,5 +1,60 @@
 package com.workday.reusables;
 
-public class Testbase {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Testbase {
+	public static WebDriver driver;
+	public static Properties prop;
+
+	/*
+	 * Here inside the Testbase constructor we are creating the object for the prop
+	 * and fileinputstream by providing the path of the config file
+	 */
+	public Testbase() {
+		try {
+			// how to read a property file
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream(
+					"E:/Bhavani/Selenium_Workday_Project/src/main/java/com/workday/config/config.properties");
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
+	/*
+	 * In Initialization method we are launching the browser by using if and else if
+	 * condition, after launching the browser user is maximizing the window and
+	 * waiting for sometime and later providing the required url by providing the config file path
+	 */
+	@SuppressWarnings("deprecation")
+	public static void initialization() {
+		String browserName = prop.getProperty("browser");
+		if (browserName.equals("chrome")) {
+			System.setProperty("WebDriver.chrome.driver", "E:/Bhavani/chromedriver-win64/chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browserName.equals("FF")) {
+			System.setProperty("WebDriver.Fire.Fox", "E:/Bhavani/chromedriver-win64/chromedriver.exe");
+			// driver = new GeekoDriver();
+		}
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+//		driver.manage().timeouts().pageLoadTimeout(TestUtile.Page_Load_Timeout, TimeUnit.SECONDS);
+//		driver.manage().timeouts().implicitlyWait(TestUtile.Implicit_wait, TimeUnit.SECONDS);
+
+		//driver.manage().timeouts().pageLoadTimeout(TestUtile.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		//driver.manage().timeouts().implicitlyWait(TestUtile.IMPLICIT_WAIT, TimeUnit.SECONDS);
+		driver.get(prop.getProperty("url"));
+	}
 }
